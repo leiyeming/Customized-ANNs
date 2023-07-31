aa6e2761f900382a3716555ae13f7b71e127f555
leiyeming <47510325+leiyeming@users.noreply.github.com>
2023-07-31 16:26:51 +1000

    update

---
+++ codes/C_ANN_fucs.py
@@ -0,0 +1,254 @@
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import r2_score, mean_squared_error
+import random
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import Adam, SGD
+from torch.nn.modules.loss import MSELoss
+import time
+
+n2t = lambda n: torch.from_numpy(n).type(torch.float32)
+def split_data(data, train_row, test_row, pred_row, pred_all_row):
+    X_tr = data.iloc[:train_row, :]
+    # test dataset
+    X_ts = data.iloc[train_row:train_row + test_row, :]
+    # prediction dataset
+    X_pred = data.iloc[train_row + test_row:train_row + test_row + pred_row, :]
+    # prediction all dataset
+    X_pred_all = data.iloc[train_row + test_row + pred_row:train_row + test_row + pred_row + pred_all_row, :]
+
+    X_tr = n2t(X_tr.values)
+    X_ts = n2t(X_ts.values)
+    X_pred = n2t(X_pred.values)
+    X_pred_all = n2t(X_pred_all.values)
+    return X_tr, X_ts, X_pred, X_pred_all
+
+def r_squared(y_true, y_pred):
+    # Compute the total sum of squares
+    ss_tot = torch.sum((y_true - torch.mean(y_true)) ** 2)
+
+    # Compute the residual sum of squares
+    ss_res = torch.sum((y_true - y_pred) ** 2)
+
+    # Compute the R-squared value
+    r2 = 1 - ss_res / ss_tot
+
+    return r2
+# define the function to normalize continue variables, here lon and lat
+def lon_lat_norm(lon_lat,data):
+    train_stats = lon_lat.describe()
+    train_stats.head()
+    train_stats = train_stats.transpose()
+    def norm(x):
+        return (x - train_stats['mean']) / train_stats['std']
+    return norm(data)
+
+# define dataset
+class DatasetWrapper_ANN_F(Dataset):
+    def __init__(self, X, y):
+        self.X, self.y = X, y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+class DatasetWrapper_ANN_S_ST(Dataset):
+        def __init__(self, X_1, X_2, y):
+            self.X_1,self.X_2, self.y = X_1, X_2, y
+
+        def __len__(self):
+            return len(self.X_1)
+
+        def __getitem__(self, idx):
+            return self.X_1[idx],self.X_2[idx], self.y[idx]
+
+# define models based on architectures
+class MLP_ANN_F(nn.Module):
+            def __init__(self, input_size_nl, hidden_sizes):
+                super(MLP_ANN_F, self).__init__()
+                self.layers = nn.ModuleList()
+                self.layers.append(nn.Linear(input_size_nl, hidden_sizes[0]))
+                self.layers.append(nn.Sigmoid())
+                for i in range(1, len(hidden_sizes)):
+                    self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
+                    self.layers.append(nn.Sigmoid())
+                self.layers.append(nn.Linear(hidden_sizes[-1], 1))
+
+            def forward(self, x_nl):
+                out = x_nl
+                for layer in self.layers:
+                    out = layer(out)
+                return out
+class MLP_ANN_S(nn.Module):
+            def __init__(self, input_size_nl, hidden_sizes,input_size_l):
+                super(MLP_ANN_S, self).__init__()
+                self.input_size_l = input_size_l
+                self.layers = nn.ModuleList()
+                self.layers.append(nn.Linear(input_size_nl, hidden_sizes[0]))
+                self.layers.append(nn.Sigmoid())
+                for i in range(1, len(hidden_sizes)):
+                    self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
+                    self.layers.append(nn.Sigmoid())
+                self.layers.append(nn.Linear(hidden_sizes[-1], 1))
+                self.layers.append(nn.Sigmoid())
+                self.concat_layer = nn.Linear(input_size_l+1, 1)
+
+            def forward(self, x_nl,x_l):
+                out = x_nl
+                for layer in self.layers:
+                    out = layer(out)
+
+                out = torch.cat((out, x_l), dim=1)
+                out = self.concat_layer(out)
+                return out
+class MLP_ANN_ST(nn.Module):
+            def __init__(self, input_size_nl, hidden_sizes,input_size_l):
+                super(MLP_ANN_ST, self).__init__()
+                self.input_size_l = input_size_l
+                self.layers = nn.ModuleList()
+                self.layers.append(nn.Linear(input_size_nl, hidden_sizes[0]))
+                self.layers.append(nn.Sigmoid())
+                for i in range(1, len(hidden_sizes)):
+                    self.layers.append(nn.Linear(hidden_sizes[i-1], hidden_sizes[i]))
+                    self.layers.append(nn.Sigmoid())
+                self.layers.append(nn.Linear(hidden_sizes[-1], 1))
+                self.layers.append(nn.Sigmoid())
+                self.concat_layer = nn.Linear(input_size_l+1, 1)
+
+            def forward(self, x_nl,x_l):
+                out = x_nl
+                for layer in self.layers:
+                    out = layer(out)
+
+                out = torch.cat((out, x_l), dim=1)
+                out = self.concat_layer(out)
+                return out
+
+# training process: train_ANN_F inout 1 dataset, train_ANN_S_ST input 2 datasets. Print model, batch loss, R2, train loss, time. R2, train loss are on validation datasets (here we used entrire training set as validation set)
+def train_ANN_F(net, optimizer, dataset, nepochs=100, batch_size=100, val_nl=None, val_y=None,device=None,scenario=None,NN_str=None,early_stopping=None,criterion=None):
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    val_nl=val_nl.to(device)
+    val_y=val_y.to(device)
+    ## move to GPU if available
+    start_time = time.time()
+    for epoch in range(nepochs):
+        if (epoch + 1) % 10 == 0:
+            end_time = time.time()
+            epoch_time = end_time - start_time
+            print(
+                'Scenario {}, {}, Epoch [{}/{}], Batch_Loss: {:.4f},R2: {:.4f},Total_Loss: {:.4f}, time: {:.4f}'.format(
+                    scenario,
+                    NN_str,
+                    epoch + 1,
+                    nepochs,
+                    loss.item(),
+                    r_squared_val,
+                    val_loss,
+                    epoch_time))
+            start_time = time.time()
+        for tr_nl, labels in dataloader:
+            tr_nl = tr_nl.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward pass
+            outputs = net(tr_nl.float())
+            loss = criterion(outputs.squeeze(), labels.float().squeeze())
+
+            # backward pass and optimization
+            loss.backward()
+            optimizer.step()
+        with torch.no_grad():
+            val_out = torch.exp(net(val_nl.float()))
+            val_loss = criterion(val_out.squeeze(), val_y.float().squeeze())
+            r_squared_val = r_squared(val_y.float().squeeze(), val_out.squeeze())
+        early_stopping(val_loss, net)
+        if early_stopping.early_stop:
+            print('Early stopping')
+            break
+def train_ANN_S_ST(net, optimizer, loss, dataset, nepochs=100, batch_size=100, val_nl=None, val_l=None,val_y=None,device=None,scenario=None,NN_str=None,early_stopping=None,criterion=None):
+                dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+                val_nl = val_nl.to(device)
+                val_l = val_l.to(device)
+                val_y = val_y.to(device)
+                ## move to GPU if available
+                start_time = time.time()
+                for epoch in range(nepochs):
+                    if (epoch + 1) % 10 == 0:
+                        end_time = time.time()
+                        epoch_time = end_time - start_time
+                        print(
+                            'Scenario {}, {}, Epoch [{}/{}], Batch_Loss: {:.4f},R2: {:.4f},Total_Loss: {:.4f}, time: {:.4f}'.format(
+                                scenario,
+                                NN_str,
+                                epoch + 1,
+                                nepochs,
+                                loss.item(),
+                                r_squared_val,
+                                val_loss,
+                                epoch_time))
+                        start_time = time.time()
+                    for tr_nl, tr_l, labels in dataloader:
+                        tr_nl = tr_nl.to(device)
+                        tr_l = tr_l.to(device)
+                        labels = labels.to(device)
+
+                        # zero the parameter gradients
+                        optimizer.zero_grad()
+
+                        # forward pass
+                        outputs = net(tr_nl.float(), tr_l.float())
+                        loss = criterion(outputs.squeeze(), labels.float().squeeze())
+
+                        # backward pass and optimization
+                        loss.backward()
+                        optimizer.step()
+                    with torch.no_grad():
+                        val_out = torch.exp(net(val_nl.float(), val_l.float()))
+                        val_loss = criterion(val_out.squeeze(), val_y.float().squeeze())
+                        r_squared_val = r_squared(val_y.float().squeeze(), val_out.squeeze())
+                    early_stopping(val_loss, net)
+                    if early_stopping.early_stop:
+                        print('Early stopping')
+                        break
+
+# early stopping for all architectures
+class EarlyStopping:
+    def __init__(self, patience=10, delta=0, verbose=True, loc=None,NN_str=None):
+        self.patience = patience
+        self.delta = delta
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.verbose = verbose
+        self.loc = loc
+        self.NN_str=str(NN_str)
+
+    def __call__(self, val_loss, model):
+        if self.best_score is None:
+            self.best_score = val_loss
+            self.save_checkpoint(val_loss, model)
+        elif val_loss > self.best_score + self.delta:
+            self.counter += 1
+            # print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = val_loss
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        if val_loss < self.val_loss_min:
+            if self.verbose:
+                print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+            torch.save(model.state_dict(), self.loc + "model/" + self.NN_str + '.pt')
+            self.val_loss_min = val_loss
---
+++ codes/spatial-temporal_ANN.py
@@ -0,0 +1,127 @@
+import sys
+sys.path.append(r'C:\Users\lei06f\Documents\GitHub\Customized-ANNs\codes')
+from C_ANN_fucs import *
+
+# set random seeds for reproducibility
+torch.manual_seed(1)
+
+# True: train a new model; False: load a trained model
+model_train = True
+
+# check if a GPU is available
+if torch.cuda.is_available():
+    device = torch.device('cuda:1')
+else:
+    device = torch.device('cpu')
+
+loc = 'C:/Users/lei06f/Documents/GitHub/Customized-ANNs/data/'
+scenario=2
+scenario=str(scenario)
+# Load train, test, and two prediction datasets from CSV files.
+# need to combine all datasets to get the same dummy variables (categorical variables onehot encoding and continue variables normalization)
+
+feature_names = ['lon', 'lat', 'yr', 'vess', 'tech_1', 'tech_2']
+cat_cols = ['vess', 'yr']
+# tr = pd.read_csv('train.csv')
+types_dict = {'yr': object}
+tr = pd.read_csv(loc + 'train.csv', dtype=types_dict)
+lon_lat = tr[["lon", "lat"]]   # normalize lon and lat
+X_tr, y_tr = tr[feature_names], tr['catch'].values
+
+ts = pd.read_csv(loc+'test.csv', dtype=types_dict)
+X_ts, y_ts = ts[feature_names], ts['catch'].values
+
+pred_data = pd.read_csv(loc+'predict_dataset.csv', dtype=types_dict)
+X_pred, y_pred= pred_data[feature_names], pred_data['catch'].values
+
+pred_data = pd.read_csv(loc+'predict_all_location.csv', dtype=types_dict)
+X_pred_all, y_pred_all= pred_data[feature_names], pred_data['catch'].values
+
+# combine all datasets
+train_row=X_tr.shape[0]
+test_row=X_ts.shape[0]
+pred_row=X_pred.shape[0]
+pred_all_row=X_pred_all.shape[0]
+
+new_data=pd.concat([pd.DataFrame(X_tr),pd.DataFrame(X_ts),pd.DataFrame(X_pred),pd.DataFrame(X_pred_all)], axis=0)
+new_data = new_data.reset_index(drop=True)
+
+# normalize continue variables (lon and lat in this case)
+new_data[["lon", "lat"]] = lon_lat_norm(lon_lat, new_data[["lon", "lat"]])
+# Conver tcategorical features to dummy features.
+
+### onehotencoder categorical variables only
+# Identify which columns are categorical
+MLP_1_col = ['lon', 'lat']
+MLP_1_col.extend('yr_' + tr['yr'].unique())
+
+cat_cols = ['vess','yr']
+# Create a OneHotEncoder object for categorical variables
+encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+new_data_encoded = encoder.fit_transform(new_data[cat_cols])
+# Assign the column names to the encoded data
+column_names = encoder.get_feature_names_out(cat_cols)
+new_data_encoded = pd.DataFrame(new_data_encoded, columns=column_names)
+
+# Combine categorical data and the continuous data
+new_data = pd.concat([pd.DataFrame(new_data_encoded), new_data.drop(cat_cols, axis=1)], axis=1)
+
+#Convert data from numpy arrays to PyTorch tensors.
+y_tr, y_ts, y_pred, y_pred_all  = n2t(y_tr).unsqueeze(1), n2t(y_ts).unsqueeze(1),  n2t(y_pred).unsqueeze(1), n2t(y_pred_all).unsqueeze(1)
+
+# split into 2 parts (one with for MLP_1 and the other for MLP_2)
+new_data_nl,new_data_l= new_data[MLP_1_col], new_data.drop(MLP_1_col, axis=1)
+
+# split into train, test, and prediction datasets
+X_tr_nl, X_ts_nl, X_pred_nl, X_pred_all_nl= split_data(new_data_nl, train_row,test_row,pred_row,pred_all_row)
+X_tr_l, X_ts_l, X_pred_l, X_pred_all_l= split_data(new_data_l, train_row,test_row,pred_row,pred_all_row)
+
+## define the model,
+# Since we only use MLP for the spatial-temporal variables, we only need to define one MLP and combine the output with the other variables.
+neuron=[16,16,16]
+print("neuron:", neuron)
+NN_str = "ANN_ST_"
+for j in neuron:
+    NN_str = NN_str + str(j) + "_"
+NN_str = NN_str[:-1]
+# layer=len(neuron)
+
+net=MLP_ANN_ST(X_tr_nl.shape[1],neuron,X_tr_l.shape[1])
+net=net.to(device)
+
+
+## define the optimizer and loss function
+# optimizer = SGD(net.parameters(), lr=1e-2)
+optimizer = Adam(net.parameters())
+criterion = nn.MSELoss()
+
+## define the dataset and early stopping
+dataset = DatasetWrapper_ANN_S_ST(X_tr_nl,X_tr_l, torch.log(y_tr))
+# early stopping
+early_stopping = EarlyStopping(patience=500, verbose=False,loc=loc,NN_str=NN_str)
+######### model training  #########
+if model_train:
+    train_ANN_S_ST(net, optimizer, dataset, batch_size=100, nepochs=8000, val_nl=X_tr_nl, val_l=X_tr_l,
+                val_y=y_tr, device=device, scenario=scenario, NN_str=NN_str, early_stopping=early_stopping,
+                criterion=criterion)
+# load best model
+model_path = loc + "model/" + str(NN_str) + '.pt'
+net.load_state_dict(torch.load(model_path))
+
+######### prediction  #########
+with torch.no_grad():
+    net.to(torch.device('cpu'))
+    pred_tr = torch.exp(net(X_tr_nl, X_tr_l))
+    pred_ts = torch.exp(net(X_ts_nl, X_ts_l))
+    pred_pred = torch.exp(net(X_pred_nl, X_pred_l))
+    pred_pred_all = torch.exp(net(X_pred_all_nl, X_pred_all_l))
+R2 = r2_score(y_tr, pred_tr)
+mse_train = mean_squared_error(y_tr, pred_tr)
+mse_test = mean_squared_error(y_ts, pred_ts)
+mse_visited = mean_squared_error(y_pred, pred_pred)
+mse_all = mean_squared_error(y_pred_all, pred_pred_all)
+
+print("mse_train:", mse_train, "mse_test:",
+      mse_test, "R2:", R2,
+      "mse_visited:", mse_visited,
+      "mse_all:", mse_all)
